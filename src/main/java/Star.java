2f4b0bc7df26b3530778bbfff2ecfdca8bd99578
WEIJIE01 <gohweijie0@gmail.com>
2026-01-26 23:36:10 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Star.java
@@ -0,0 +1,12 @@
+public class Star {
+    public static void main(String[] args) {
+        String chatbot_name = "Star";
+        String outro = """
+                
+                What can I do for you?
+                
+                Bye. Hope to see you again soon!
+                """;
+        System.out.println("\nHello! I'm " + chatbot_name + outro);
+    }
+}
