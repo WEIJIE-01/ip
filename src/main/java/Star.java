f06dd8bcabcebd06f726123ecd42f8118e8a5d68
WEIJIE01 <gohweijie0@gmail.com>
2026-01-30 16:40:11 +0800

    A-TextUiTesting: Automated Text UI Testing

--- src/main/java/Star.java
+++ src/main/java/Star.java
@@ -2,21 +2,24 @@ public class Star {
     public static void main(String[] args) {
         Ui.printWelcomeMessage();   // Prints welcome message in UI
 
-        while (Ui.hasMoreInput()) {
-            String input = (Ui.scanInput());    // Scans for user input till "bye" received
-            if (input.isEmpty()) {
+        while (true) {  // Infinite loop
+            if (!Ui.hasMoreInput()) {
+                break;  // EOF → exit
+            }
+
+            // Ignores empty input
+            String input = Ui.scanInput();
+            if (input.isEmpty() || input.trim().isEmpty()) {
                 continue;
             }
-            else {
-                Message message = new Message(input);
-                message.parseMessage(); // splits message into tokens
 
-                // exits when "bye" is the first token
-                if (LogicController.run(message)) {
-                    break;
-                }
+            Message message = new Message(input);
+            message.parseMessage();
+
+            // exit ONLY for "bye"
+            if (LogicController.run(message)) {
+                break;
             }
-
         }
     }
 }
