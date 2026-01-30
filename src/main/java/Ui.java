9457931dc85c7f2e1e8f30a9477fab31488d9421
WEIJIE01 <gohweijie0@gmail.com>
2026-01-30 16:40:11 +0800

    A-TextUiTesting: Automated Text UI Testing

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -7,6 +7,7 @@ public class Ui {
 
     public static final String CHATBOT_NAME = "Star";
     public static final String BOT_LABEL = String.format("[%s]", CHATBOT_NAME);
+    public static final String INDENTATION = "       ";
     private static final Scanner sc = new Scanner(System.in);
 
     // Prints welcome message.
@@ -28,35 +29,35 @@ public class Ui {
 
     // Prints goodbye message.
     public static void printExitMessage(){
-        System.out.println(BOT_LABEL + "Bye. Hope to see you again soon!");
+        System.out.printf("\n%s Bye. Hope to see you again soon!",BOT_LABEL);
     }
 
     // Prints all task
     public static void printTasks() {
+        System.out.println("\n" + BOT_LABEL + " Tasklist:");
         for (int i = 0; i < TaskList.getSize(); i++) {
             Task task = TaskList.getTask(i);
-            System.out.printf("%d.%s\n", i + 1, task.toString());
+            System.out.printf("%s%d.%s\n",INDENTATION, i + 1, task.toString());
         }
-        System.out.println(" ");
     }
 
 
     public static void printString(String msg) {
-        System.out.println(msg);
+        System.out.println(INDENTATION + msg);
     }
 
 
     public static void printNewTask(Task task) {
-        System.out.printf("%s I have added a new task: %s\n", BOT_LABEL, task.name);
+        System.out.printf("\n%s I have added a new task: %s\n", BOT_LABEL, task.name);
     }
 
     public static void showMarkSuccess(Task task) {
-        System.out.println(BOT_LABEL + " Nice! I've marked this task as done:");
+        System.out.printf("\n%s Nice! I've marked this task as done:",BOT_LABEL);
         System.out.println(task.toString());
     }
 
     public static void showUnmarkSuccess(Task task) {
-        System.out.println(BOT_LABEL + " OK, I've marked this task as not done yet:");
+        System.out.printf("\n%s OK, I've marked this task as not done yet:",BOT_LABEL);
         System.out.println(task.toString());
     }
 }
