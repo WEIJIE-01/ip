0112a2a6cc1f7bfc803a06c3f43fc85481da73d4
WEIJIE01 <gohweijie0@gmail.com>
2026-02-08 00:53:46 +0800

    Added JUnit test for DateTimeConverter

---
+++ src/test/java/parser/DateTimeConverterTest.java
@@ -0,0 +1,22 @@
+package parser;
+
+import exception.CustomException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+/**
+ * Tests DateTimeConverterTest by giving invalid Month
+ */
+public class DateTimeConverterTest {
+    @Test
+    public void toLocalDate_invalidDate_throwsError() {
+        CustomException ex = assertThrows(
+            CustomException.class,
+            () -> DateTimeConverter.toLocalDate(new String[]{"2026-47-30"})
+        );
+        assertEquals("Invalid date time format! Input yyyy-MM-dd HH:mm", ex.getMessage());
+    }
+}
