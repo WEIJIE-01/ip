243232e7f682d956e07a744f4141af9667d7f7c6
WEIJIE01 <gohweijie0@gmail.com>
2026-02-08 10:52:02 +0800

    Create header comments to all methods

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import model.TaskList;
 import model.Task;
 /**
- * Interacts with user by printing
+ * Interacts with user by printing.
  */
 public class Ui {
 
@@ -14,7 +14,9 @@ public class Ui {
     public static final String INDENTATION = "       ";
     private static Scanner sc = new Scanner(System.in);
 
-    // prints welcome message.
+    /**
+     * Prints welcome message.
+     */
     public static void printWelcomeMessage() {
 
         String WELCOME_MESSAGE = BOT_LABEL + " Hello! I'm "
@@ -22,23 +24,32 @@ public class Ui {
         System.out.println(WELCOME_MESSAGE);
     }
 
-    // scans for user input.
+    /**
+     * Scans for user input.
+      */
     public static String scanInput() {
         return sc.nextLine();
     }
 
-    // accounts for multiple line inputs
-    // runs line by line
+    /**
+     * accounts for multiple line inputs
+     * runs line by line
+     * @return nextline String
+     */
     public static boolean hasMoreInput() {
         return sc.hasNextLine();
     }
 
-    // prints goodbye message.
+    /**
+     * Prints goodbye message.
+     */
     public static void printExitMessage() {
         System.out.printf("\n%s Bye. Hope to see you again soon!", BOT_LABEL);
     }
 
-    // prints all task
+    /**
+     * Prints all task.
+     */
     public static void printTasks() {
         System.out.println("\n" + BOT_LABEL + " Tasklist:");
         for (int i = 0; i < TaskList.getSize(); i++) {
@@ -47,38 +58,52 @@ public class Ui {
         }
     }
 
-    // prints any string
+    /**
+     * Prints any string.
+     */
     public static void printString(String msg) {
         System.out.println(INDENTATION + msg);
     }
 
-    // prints error message
+    /**
+     * Prints error message.
+     */
     public static void printErrorMsg(String errorMsg) {
         System.out.println("\n" + BOT_LABEL + " ERROR! " + errorMsg);
     }
 
-    // prints to inform new task added
+    /**
+     * Prints to show status of new task added.
+     */
     public static void printNewTask(Task task) {
         System.out.printf("\n%s I have added a new task: %s\n", BOT_LABEL, task.name);
     }
 
-    // prints to inform taskList has been saved
+    /**
+     * Prints to show TaskList is saved.
+     */
     public static void printSave() {
         System.out.printf("\n%s Tasks Saved\n", BOT_LABEL);
     }
 
-    // prints to inform task has been deleted
+    /**
+     * Prints to show task removed.
+     */
     public static void printDeleteTask(Task task) {
         System.out.printf("\n%s I have removed this task: %s\n", BOT_LABEL, task.name);
     }
 
-    // prints to inform task has been marked to done
+    /**
+     * Prints to show status of marked Task.
+     */
     public static void showMarkSuccess(Task task) {
         System.out.printf("\n%s Nice! I've marked this task as done:",BOT_LABEL);
         System.out.println(task.toString());
     }
 
-    // prints to inform task has been unmarked to not done
+    /**
+     * Prints to show status of unmarked Task.
+     */
     public static void showUnmarkSuccess(Task task) {
         System.out.printf("\n%s OK, I've marked this task as not done yet:",BOT_LABEL);
         System.out.println(task.toString());
